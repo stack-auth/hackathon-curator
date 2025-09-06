4ae6cc4b743bcc86512156c2c7896624b8c78b9f
Bilal Godil <bg2002@gmail.com>
2025-09-06 13:00:51 -0700

    small fixes

--- curator/src/extension.ts
+++ curator/src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	const disposable = vscode.commands.registerCommand('curator.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from extension!');
+		vscode.window.showInformationMessage('Hello World from extension v2!');
 	});
 
 	context.subscriptions.push(disposable);
